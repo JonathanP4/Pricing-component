629b3d2ee09f8c714cc85174524c202c8fef6aae
Jonathan <jonathanpetersen2016@gmail.com>
2023-02-13 17:56:08 -0300

    Fix range input

--- app/script/index.js
+++ app/script/index.js
@@ -1,26 +1,24 @@
 const time = document.querySelector('#time');
 const prices = document.querySelectorAll('.price');
+const sliderThumb = document.querySelector('.slider-thumb')
 
 const basePrices = [19.99, 24.99, 39.99]
-
-time.value = 2
 
 if (window.innerHeight > 733) {
    document.body.style.height = `${window.innerHeight}px`
 }
 
-const setPrices = val => {
-   console.log(val);
+const setPrices = cond => {
+   console.log(cond);
    prices.forEach((p, i) => {
-      p.textContent = `${val === 1 ? ((basePrices[i] * 10) + 0.09).toFixed(2) : basePrices[i]}`
+      p.textContent = `${cond === true ? ((basePrices[i] * 10) + 0.09).toFixed(2) : basePrices[i]}`
    });
 }
-
 time.addEventListener('click', () => {
-   setPrices(+time.value)
-})
-time.addEventListener('touchend', () => {
-   setPrices(+time.value)
+   sliderThumb.classList.toggle('slider-thumb--active')
+   const cond = sliderThumb.classList.contains('slider-thumb--active')
+
+   setPrices(cond)
 })
 window.addEventListener('resize', () => {
    if (window.innerHeight > 733) {
